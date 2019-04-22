7cf4f5d8bce5244c91719a432827ec0c20549eb2
Julien Mialon <mialon.julien@gmail.com>
2019-04-22 15:48:23 +0200

    Add unit tests for non special items

--- csharpcore/GildedRoseTest.cs
+++ csharpcore/GildedRoseTest.cs
@@ -6,12 +6,99 @@ namespace csharpcore
     public class GildedRoseTest
     {
         [Fact]
-        public void foo()
+        public void GivenAnItem_CheckThatNameDoesNotChange()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
+            Item item = new Item
+            {
+                Name = "foo",
+                SellIn = 100,
+                Quality = 40
+            };
+
+            RunForItem(item);
+            
+            Assert.Equal("foo", item.Name);
+        }
+
+        [Fact]
+        public void GivenNormalItem_CheckThatSellInDateDecrease()
+        {
+            Item item = new Item
+            {
+                Name = "foo",
+                SellIn = 100,
+                Quality = 40
+            };
+
+            RunForItem(item);
+            
+            Assert.Equal(99, item.SellIn);
+        }
+        
+        [Fact]
+        public void GivenNormalItem_CheckThatQualityDecreaseByOneBeforeExpire()
+        {
+            Item item = new Item
+            {
+                Name = "foo",
+                SellIn = 100,
+                Quality = 40
+            };
+
+            RunForItem(item);
+            
+            Assert.Equal(39, item.Quality);
+        }
+        
+        [Fact]
+        public void GivenNormalItem_CheckThatQualityDecreaseTwiceAsFastAfterExpire()
+        {
+            Item item = new Item
+            {
+                Name = "foo",
+                SellIn = 0,
+                Quality = 40
+            };
+
+            RunForItem(item);
+            
+            Assert.Equal(38, item.Quality);
+        }
+        
+        [Fact]
+        public void GivenNormalItem_CheckThatQualityCantBeLessThanZero()
+        {
+            Item item = new Item
+            {
+                Name = "foo",
+                SellIn = 100,
+                Quality = 0
+            };
+
+            RunForItem(item);
+            
+            Assert.Equal(0, item.Quality);
+        }
+        
+        [Fact]
+        public void GivenNormalItem_CheckThatSellInDateCanBeLessThanZero()
+        {
+            Item item = new Item
+            {
+                Name = "foo",
+                SellIn = 0,
+                Quality = 40
+            };
+
+            RunForItem(item);
+            
+            Assert.Equal(-1, item.SellIn);
+        }
+        
+        private void RunForItem(Item item)
+        {
+            GildedRose app = new GildedRose(new List<Item> {item});
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
         }
     }
 }
