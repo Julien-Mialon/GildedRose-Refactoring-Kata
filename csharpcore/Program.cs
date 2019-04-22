e4eaadf1c87a490e043caad904652f4643005fd7
Julien Mialon <mialon.julien@gmail.com>
2019-04-22 17:40:45 +0200

    code cleanup

--- csharpcore/Program.cs
+++ csharpcore/Program.cs
@@ -9,7 +9,8 @@ namespace csharpcore
         {
             Console.WriteLine("OMGHAI!");
 
-            IList<Item> Items = new List<Item>{
+            IList<Item> items = new List<Item>
+            {
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
@@ -33,21 +34,21 @@ namespace csharpcore
                     SellIn = 5,
                     Quality = 49
                 },
-				// this conjured item does not work properly yet
-				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 16}
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 16}
             };
 
-            var app = new GildedRose(Items);
+            var app = new GildedRose(items);
 
 
             for (var i = 0; i < 31; i++)
             {
                 Console.WriteLine("-------- day " + i + " --------");
                 Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
+                foreach (Item item in items)
                 {
-                    System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
+                    Console.WriteLine(item.Name + ", " + item.SellIn + ", " + item.Quality);
                 }
+
                 Console.WriteLine("");
                 app.UpdateQuality();
             }
